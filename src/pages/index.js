545f62750ae40ae71c783596cd0f7eaecda0a86f
mcs133 <mcs133@DCSRCC.com>
2023-10-12 14:45:01 +0530

    first

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+   <>
+   <div className="bg-red-500 flex items-center justify-center w-screen h-screen">
+      <h2 className="text-6xl">Tumhari mummy </h2>
+   </div>
+   </>
+  )
+}
